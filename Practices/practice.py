1a4a5db4fbe474143fb9d7b95ce42707e7d8af21
CodeWithDanyal <danyal.devpro@gmail.com>
2025-01-15 19:25:16 +0500

    Initial commit

---
+++ Practices/practice.py
@@ -0,0 +1,7 @@
+for i in range(0, 100, 1):
+    if i % 3 == 0:
+        print("Fizz 😎")
+    elif i % 5 == 0:
+        print("Buzz 😪")
+    elif i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz 🥶")
